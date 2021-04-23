31c26744e5d59dad12c0cd983b3fd87ed75611ae
mfrydrychowicz <mamgofry@gmail.com>
2021-04-24 01:18:14 +0200

    video actions

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -2,12 +2,14 @@ import firebase from 'firebase';
 import { ReactElement } from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import RoomsList from '../../components/RoomsList/RoomsList';
+import VideoActions from '../../components/VideoActions/VideoActions';
 
 export const Home = (): ReactElement => {
     return (
         <>
             <h1>This is an example app</h1>
             <RoomsList />
+            <VideoActions />
         </>
     );
 };
