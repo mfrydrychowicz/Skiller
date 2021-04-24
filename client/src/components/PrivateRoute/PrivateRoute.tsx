8fd5856988c37a28a14b66e11f6938d74ecef733
CaptainObjective <marcinwos18@gmail.com>
2021-04-24 17:32:02 +0200

    routy

--- client/src/components/PrivateRoute/PrivateRoute.tsx
+++ client/src/components/PrivateRoute/PrivateRoute.tsx
@@ -5,6 +5,13 @@ import { Spinner } from '@chakra-ui/react';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
     const [user, loading, error] = useAuthState(auth);
+    console.log('🚀 ~ file: PrivateRoute.tsx ~ line 8 ~ PrivateRoute ~ error', error);
+    console.log('🚀 ~ file: PrivateRoute.tsx ~ line 8 ~ PrivateRoute ~ loading', loading);
+    console.log('🚀 ~ file: PrivateRoute.tsx ~ line 8 ~ PrivateRoute ~ user', user);
+
+    if (loading) {
+        return <Spinner />;
+    }
 
     if (error) {
         console.log(error);
