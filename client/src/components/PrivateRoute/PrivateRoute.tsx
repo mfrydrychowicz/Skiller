4da3bb4d3d17c2e6049b228b6caa1994a0eabd25
CaptainObjective <marcinwos18@gmail.com>
2021-04-24 17:54:16 +0200

    DziaLA nie ruszac!!!!!!

--- client/src/components/PrivateRoute/PrivateRoute.tsx
+++ client/src/components/PrivateRoute/PrivateRoute.tsx
@@ -5,9 +5,6 @@ import { Spinner, Box } from '@chakra-ui/react';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
     const [user, loading, error] = useAuthState(auth);
-    console.log('🚀 ~ file: PrivateRoute.tsx ~ line 8 ~ PrivateRoute ~ error', error);
-    console.log('🚀 ~ file: PrivateRoute.tsx ~ line 8 ~ PrivateRoute ~ loading', loading);
-    console.log('🚀 ~ file: PrivateRoute.tsx ~ line 8 ~ PrivateRoute ~ user', user);
 
     if (loading)
         return (
