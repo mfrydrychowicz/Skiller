7e54ac78a7a8d7c2d932c81ff95e76f4400fa3a8
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-04-24 03:44:48 +0200

    Add list of films

---
+++ client/src/components/RoomsList/RoomCard/RoomCard.style.ts
@@ -0,0 +1,21 @@
+const styles = {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "gray.600",
+    textAlign: 'center',
+    padding: "25px 0",
+    fontFamily: "sans-serif",
+}
+
+export const cardStyle = {
+    '.card': {
+        transform: 'scale(0.8)'
+    },
+
+    '.card:hover': {
+        transform: 'scale(1)'
+    }
+}
+
+export default styles
