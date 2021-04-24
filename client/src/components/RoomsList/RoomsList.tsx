0c879d7a022e7022654b6a307205f10aa5c0a16f
Asia Zioła <ziola.jm@gmail.com>
2021-04-24 08:52:15 +0200

    new video

--- client/src/components/RoomsList/RoomsList.tsx
+++ client/src/components/RoomsList/RoomsList.tsx
@@ -1,28 +1,44 @@
 import {
-    Box, Button, ButtonGroup, Divider, Drawer,
+    Box,
+    Button,
+    ButtonGroup,
+    Divider,
+    Drawer,
     DrawerBody,
     DrawerCloseButton,
-    DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Flex, Input, Spinner, useDisclosure
+    DrawerContent,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    Flex,
+    Input,
+    Spinner,
+    useDisclosure
 } from '@chakra-ui/react';
-import {css} from '@emotion/react';
+import { css } from '@emotion/react';
 import firebase from 'firebase';
-import {useState, useRef} from 'react';
-import {useCollection} from 'react-firebase-hooks/firestore';
-import {Link, useHistory} from 'react-router-dom';
-import {newRoom} from '../../db/newRoom';
+import { useState, useRef, useEffect, MutableRefObject } from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { Link, useHistory } from 'react-router-dom';
+import { newRoom } from '../../db/newRoom';
 import RoomCard from './RoomCard/RoomCard';
-import {AddIcon, CloseIcon, CheckIcon} from '@chakra-ui/icons'
+import { AddIcon, CloseIcon, CheckIcon } from '@chakra-ui/icons';
+import socket from '../../socket';
 
-const RoomsList = () => {
+const RoomsList = (props) => {
     const [roomName, setRoomName] = useState('');
     const history = useHistory();
     const [rooms, loading, error] = useCollection(firebase.firestore().collection('Rooms'), {
-        snapshotListenOptions: {includeMetadataChanges: true}
+        snapshotListenOptions: { includeMetadataChanges: true }
     });
 
-    const {isOpen, onOpen, onClose} = useDisclosure()
-    const btnRef = useRef()
+    const roomRef = useRef() as MutableRefObject<any>;
+    const userRef = useRef() as MutableRefObject<any>;
+    const [err, setErr] = useState(false);
+    const [errMsg, setErrMsg] = useState('');
 
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const btnRef = useRef();
 
     const handleRoomNameChange = (event: any) => {
         setRoomName(event.target.value);
@@ -32,66 +48,91 @@ const RoomsList = () => {
         setRoomName('');
         const id = await newRoom(roomName);
         console.log('🚀 ~ file: RoomsList.tsx ~ line 23 ~ handleRoomNameSubmit ~ id', id);
-        history.push(`/room/${id}`, {isHost: true});
+        history.push(`/room/${id}`, { isHost: true });
     };
 
-    if (loading) return <Spinner/>;
+    useEffect(() => {
+        socket.on('FE-error-user-exist', ({ error }) => {
+            if (!error) {
+                const roomName = roomRef.current.value;
+                const userName = userRef.current.value;
 
+                sessionStorage.setItem('user', userName);
+                props.history.push(`/room/${roomName}`);
+            } else {
+                setErr(error);
+                setErrMsg('User name already exist');
+            }
+        });
+    }, [props.history]);
+
+    function clickJoin() {
+        const roomName = 'test';
+        const userName = 'asia';
+
+        if (!roomName || !userName) {
+            setErr(true);
+            setErrMsg('Enter Room Name or User Name');
+        } else {
+            socket.emit('BE-check-user', { roomId: roomName, userName });
+        }
+    }
+
+    if (loading) return <Spinner />;
 
     return (
         <>
-            <Flex wrap='wrap' maxWidth='100%' justify='center'>
-                {rooms.docs.map((room) =>
-                    <Box m='50px' key={room.id}>
+            <Flex wrap="wrap" maxWidth="100%" justify="center">
+                {rooms.docs.map((room) => (
+                    <Box m="50px" key={room.id} ref={roomRef} onClick={clickJoin}>
                         <Link to={`/room/${room.id}`}>
-                            <RoomCard id={room.id} name={room.data().name}/>
+                            <RoomCard id={room.id} name={room.data().name} />
                         </Link>
                     </Box>
-                )
-                }
+                ))}
             </Flex>
-            <Button leftIcon={<AddIcon/>} sx={{position: 'absolute', bottom: '25px', right: '35px', zIndex: '10'}}
-                    onClick={onOpen}> Add New Room</Button>
-            <Drawer
-                isOpen={isOpen}
-                placement="right"
-                onClose={onClose}
-                finalFocusRef={btnRef}
+            jasne
+            <Button
+                leftIcon={<AddIcon />}
+                sx={{ position: 'absolute', bottom: '25px', right: '35px', zIndex: '10' }}
+                onClick={onOpen}
             >
+                {' '}
+                Add New Room
+            </Button>
+            <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={btnRef}>
                 <DrawerOverlay>
                     <DrawerContent>
-
-
                         <DrawerHeader>Room name</DrawerHeader>
 
-                        <DrawerBody m='50px 0 '>
+                        <DrawerBody m="50px 0 ">
                             <Input
                                 placeholder="Your new room"
-                                onChange={e => setRoomName(e.target.value)}
+                                onChange={(e) => setRoomName(e.target.value)}
                                 isRequired
                             />
                         </DrawerBody>
 
-                        <DrawerCloseButton/>
+                        <DrawerCloseButton />
 
                         <DrawerFooter>
                             <ButtonGroup>
                                 <Button
                                     leftIcon={<CheckIcon />}
-                                    variant='solid'
-                                    colorScheme='blue'
+                                    variant="solid"
+                                    colorScheme="blue"
                                     onClick={onClose}
-                                    align='baseline'
-                                >Add</Button>
+                                    align="baseline"
+                                >
+                                    Add
+                                </Button>
                             </ButtonGroup>
                         </DrawerFooter>
-
                     </DrawerContent>
                 </DrawerOverlay>
             </Drawer>
         </>
-    )
-
+    );
 };
 
 export default RoomsList;
--- server/src/server.tsx
+++ server/src/server.tsx
@@ -41,45 +41,78 @@ const io = require('socket.io')(server, {
     }
 });
 
-const users = {};
-
-const socketToRoom = {};
+let socketList = {};
 
 io.on('connection', (socket) => {
-    socket.on('join room', (roomID) => {
-        if (users[roomID]) {
-            const length = users[roomID].length;
-            if (length === 4) {
-                socket.emit('room full');
-                return;
-            }
-            users[roomID].push(socket.id);
-        } else {
-            users[roomID] = [socket.id];
-        }
-        socketToRoom[socket.id] = roomID;
-        const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
-
-        socket.emit('all users', usersInThisRoom);
-    });
-
-    socket.on('sending signal', (payload) => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-    });
-
-    socket.on('returning signal', (payload) => {
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-    });
+    console.log(`New User connected: ${socket.id}`);
 
     socket.on('disconnect', () => {
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-        if (room) {
-            room = room.filter((id) => id !== socket.id);
-            users[roomID] = room;
+        socket.disconnect();
+        console.log('User disconnected!');
+    });
+
+    socket.on('BE-check-user', ({ roomId, userName }) => {
+        let error = false;
+
+        io.sockets.in(roomId).clients((err, clients) => {
+            clients.forEach((client) => {
+                if (socketList[client] == userName) {
+                    error = true;
+                }
+            });
+            socket.emit('FE-error-user-exist', { error });
+        });
+    });
+
+    socket.on('BE-join-room', ({ roomId, userName }) => {
+        socket.join(roomId);
+        socketList[socket.id] = { userName, video: true, audio: true };
+
+        io.sockets.in(roomId).clients((err, clients) => {
+            try {
+                const users = [];
+                clients.forEach((client) => {
+                    users.push({ userId: client, info: socketList[client] });
+                });
+                socket.broadcast.to(roomId).emit('FE-user-join', users);
+            } catch (e) {
+                io.sockets.in(roomId).emit('FE-error-user-exist', { err: true });
+            }
+        });
+    });
+
+    socket.on('BE-call-user', ({ userToCall, from, signal }) => {
+        io.to(userToCall).emit('FE-receive-call', {
+            signal,
+            from,
+            info: socketList[socket.id]
+        });
+    });
+
+    socket.on('BE-accept-call', ({ signal, to }) => {
+        io.to(to).emit('FE-call-accepted', {
+            signal,
+            answerId: socket.id
+        });
+    });
+
+    socket.on('BE-send-message', ({ roomId, msg, sender }) => {
+        io.sockets.in(roomId).emit('FE-receive-message', { msg, sender });
+    });
+
+    socket.on('BE-leave-room', ({ roomId, leaver }) => {
+        delete socketList[socket.id];
+        socket.broadcast.to(roomId).emit('FE-user-leave', { userId: socket.id, userName: [socket.id] });
+        io.sockets.sockets[socket.id].leave(roomId);
+    });
+
+    socket.on('BE-toggle-camera-audio', ({ roomId, switchTarget }) => {
+        if (switchTarget === 'video') {
+            socketList[socket.id].video = !socketList[socket.id].video;
+        } else {
+            socketList[socket.id].audio = !socketList[socket.id].audio;
         }
-        socket.broadcast.emit('user left', socket.id);
-        console.log(`Client ${socket.id} disconnected`);
+        socket.broadcast.to(roomId).emit('FE-toggle-camera', { userId: socket.id, switchTarget });
     });
 });
 
